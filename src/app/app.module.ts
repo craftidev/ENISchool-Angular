b480e05224ac7140f88f62ba1f80c91b48655103
craftidev <ivann.codev@gmail.com>
2023-10-18 14:08:38 +0000

    add linting but still pb with generating files getting 2 space indentation... next time I need to create an Angular project with ESLint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { MessageListComponent } from './components/message-list/message-list.component';
 import { MessageInputComponent } from './components/message-input/message-input.component';
+import { TestComponent } from './components/test/test.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         UserListComponent,
         MessageListComponent,
-        MessageInputComponent
+        MessageInputComponent,
+        TestComponent
     ],
     imports: [
         BrowserModule
