22a3ed03b10d799eb5bee08f51c11ac1a31091ae
craftidev <ivann.codev@gmail.com>
2023-10-18 13:53:47 +0000

    boiler plates components/services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { MessageListComponent } from './components/message-list/message-list.component';
+import { MessageInputComponent } from './components/message-input/message-input.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        UserListComponent,
+        MessageListComponent,
+        MessageInputComponent
     ],
     imports: [
         BrowserModule
